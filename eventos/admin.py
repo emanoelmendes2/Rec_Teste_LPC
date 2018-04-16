cec04a6a555b2066e3cc845c13860b1690ef0bc8
Emanoel <emmmagalhaes@gmail.com>
2018-04-16 01:15:03 -0300

    primeiro commit

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+
+
+admin.site.register(Pessoa)
+admin.site.register(Evento)
+admin.site.register(EventoCientifico)
+admin.site.register(PessoaFisica)
+admin.site.register(PessoasJuridica)
+admin.site.register(Autor)
+admin.site.register(ArtigoCientifico)
+admin.site.register(Avaliador)
+admin.site.register(Avaliacao)
+
+
 
 # Register your models here.
--- eventos/models.py
+++ eventos/models.py
@@ -2,9 +2,79 @@ from django.db import models
 
 
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=128)
-    descricao = models.TextField()
-    data_nascimento = models.DateField(blank=True, null=True)
+    nome = models.CharField(max_length=128, null=True)
+    email = models.CharField(max_length=60, null=True)
+
 
     def __str__(self):
-        return self.nome
+        return (self.nome)
+
+class Evento (models.Model):
+    nome = models.CharField(max_length=150, null=True)
+    eventoPrincipal = models.CharField(max_length=30,null=True)
+    sigla = models.CharField(max_length=10,null=True)
+    dataEHoraDeInicio = models.DateTimeField(blank=False, null=True)
+    logoTipo = models.CharField(max_length=200,null=True)
+    realisador = models.ForeignKey(Pessoa, on_delete=models.CASCADE, null=True)
+    cidade = models.CharField(max_length=30,null=True)
+    uf= models.CharField(max_length=2,null=True)
+    cep = models.CharField(max_length=8,null=True)
+
+    def __str__(self):
+        return (self.nome)
+        
+
+class EventoCientifico(Evento):
+    issn=models.CharField(max_length=30, null=True )
+
+    def __str__(self):
+        return ( self.issn)
+
+class Autor (Pessoa):
+    curriculo = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return (self.curriculo)
+
+
+class PessoasJuridica(Pessoa):
+    cnpj = models.CharField(max_length=12, null=True)
+    razaoSocial = models.CharField(max_length=100, null=True)
+
+    def __str__ (self):
+        return (self.razaoSocial)
+
+class PessoaFisica(Pessoa):
+    cpf= models.CharField(max_length=11,null=True)
+
+    def __str__(self):
+        return (self.cpf )
+
+class ArtigoCientifico(models.Model):
+    titulo = models.CharField(max_length=30, null=True)
+    autores = models.ManyToManyField(Autor, null=True)
+    evento = models.ForeignKey(EventoCientifico, on_delete=models.CASCADE, related_name='eventoCientifico', null=True)
+
+    def __str__(self):
+        return (self.titulo)
+
+
+class Avaliador (PessoaFisica):
+    formacao=models.CharField('formacao', max_length=50,null=True)
+
+    def __str__(self):
+        return (self.formacao)
+
+class Avaliacao (models.Model):
+    qualitecnica=models.IntegerField()
+    inovacao=models.IntegerField()
+    resultados=models.IntegerField()
+    metodologia=models.IntegerField()
+    adequacaoTematica=models.IntegerField()
+    avaliador=models.ForeignKey(Avaliador, null = True,blank = False,on_delete = models.CASCADE )
+    artigoAvaliacao = models.ManyToManyField(ArtigoCientifico)
+    media=models.CharField(max_length=30,null=True)
+
+    def __str__(self):
+        return (self.media)
+
